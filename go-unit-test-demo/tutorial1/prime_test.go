50c30f341f77ace52bcc7f38e9ddfb6402bb4e7b
Denes-cilwal <dinesh.silwal@wesionary.team>
2022-01-10 20:24:10 +0545

    multiple testcase for same func

--- go-unit-test-demo/tutorial1/prime_test.go
+++ go-unit-test-demo/tutorial1/prime_test.go
@@ -1,6 +1,9 @@
 package tutorial1
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestCheckIfPrime(test *testing.T) {
 	input := 89
@@ -9,4 +12,29 @@ func TestCheckIfPrime(test *testing.T) {
 	if output != expected {
 		test.Logf("Expected %v, got %v", expected, output)
 	}
+}
+
+// multiple test case on same function
+
+func TestCheckIfPrimeTable(test *testing.T) {
+
+	testCases := []struct {
+		input    int
+		expected bool
+	}{
+		{2, true},
+		{30, true},
+		{41, true},
+		{59, true},
+	}
+	for _, testCase := range testCases {
+		test.Run(fmt.Sprintf("%v", testCase.input), func(t *testing.T) {
+			got := CheckIfPrime(testCase.input)
+			if got != testCase.expected {
+				test.Logf("Expected %v, got %v", testCase.expected, testCase.input)
+
+			}
+		})
+	}
+
 }
