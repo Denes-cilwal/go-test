64bf1236ef02b4e22a9f78feafa5b51fdc65b0d8
Denes-cilwal <dinesh.silwal@wesionary.team>
2022-01-11 17:15:21 +0545

    test coverage result

---
+++ go-unit-test-demo/tutorial_1/prime.go
@@ -0,0 +1,16 @@
+package tutorial_1
+
+//CheckIfPrime
+func CheckIfPrime(num int) bool {
+	result := true
+	if num < 2 {
+		return false
+	}
+	for i := 2; i < num; i++ {
+		if num%i == 0 {
+			result = false
+			break
+		}
+	}
+	return result
+}
---
+++ go-unit-test-demo/tutorial_1/prime_test.go
@@ -0,0 +1,40 @@
+package tutorial_1
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCheckIfPrime(test *testing.T) {
+	input := 89
+	expected := true
+	output := CheckIfPrime(input)
+	if output != expected {
+		test.Logf("Expected %v, got %v", expected, output)
+	}
+}
+
+// multiple test case on same function
+
+func TestCheckIfPrimeTable(test *testing.T) {
+
+	testCases := []struct {
+		input    int
+		expected bool
+	}{
+		{2, false},
+		{30, true},
+		{41, true},
+		{59, true},
+	}
+	for _, testCase := range testCases {
+		test.Run(fmt.Sprintf("%v", testCase.input), func(t *testing.T) {
+			got := CheckIfPrime(testCase.input)
+			if got != testCase.expected {
+				test.Logf("Expected %v, got %v", testCase.expected, testCase.input)
+
+			}
+		})
+	}
+
+}
